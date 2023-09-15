678d5cbdfc3b55cd9f07808adc78fad7ddcd3283
susmp <116519843+susmp@users.noreply.github.com>
2023-09-15 17:23:53 +0300

    Add files via upload

---
+++ PySpark_test/method.py
@@ -0,0 +1,8 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+def get_dataframe(products, categories):
+    spark = SparkSession.builder.appName("ProductCategory").getOrCreate()
+    result_df = products.join(categories, on="CategoryName", how="left")
+    result_df = result_df.fillna("No Category", subset=["ProductName"])
+    return result_df.select("ProductName", "CategoryName")
